53b63f082e9107910ca7c843bcf78c5c7d966eff
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-03-02 06:22:31 +0000

    ready to deploy

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { FeaturedPosts } from '../sections/index'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
+import Head from 'next/head'
 
 export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>BLOGEDD</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
       <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
